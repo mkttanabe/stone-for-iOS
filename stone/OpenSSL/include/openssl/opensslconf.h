851a73cdd9cea4c33758b8fa947cdb112dbe0bf4
TANABE Makoto <mkttanabe2012@gmail.com>
2014-04-12 06:21:08 +0900

    OpenSSL 1.0.1e -> 1.0.1g

--- stone/OpenSSL/include/openssl/opensslconf.h
+++ stone/OpenSSL/include/openssl/opensslconf.h
@@ -86,8 +86,8 @@
 
 #if !(defined(VMS) || defined(__VMS)) /* VMS uses logical names instead */
 #if defined(HEADER_CRYPTLIB_H) && !defined(OPENSSLDIR)
-#define ENGINESDIR "/Users/t/dev/OpenSSL_build/bin/iPhoneSimulator7.0-i386.sdk/lib/engines"
-#define OPENSSLDIR "/Users/t/dev/OpenSSL_build/bin/iPhoneSimulator7.0-i386.sdk"
+#define ENGINESDIR "/Users/t/dev/OpenSSL-for-iPhone101g/bin/iPhoneSimulator7.1-i386.sdk/lib/engines"
+#define OPENSSLDIR "/Users/t/dev/OpenSSL-for-iPhone101g/bin/iPhoneSimulator7.1-i386.sdk"
 #endif
 #endif
 
